b6632a9456f9015ea1837781b7387746ddf24b0b
iam4722202468 <aceparent@gmail.com>
2019-07-18 23:22:12 -0400

    Fix problem

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int comp(int in1, int in2) {
   return in1 - in2;
 }
 
-int fitness(std::vector<std::vector<int>> *arrs, std::vector<int> currPath, int pathNum) {
+int fit(std::vector<std::vector<int>> *arrs, std::vector<int> currPath, int pathNum) {
   int acc = 0;
   for (unsigned int x = 0; x < currPath.size(); ++x) {
     acc += arrs->at(x)[currPath.at(x)];
@@ -31,19 +31,15 @@ std::vector<int> getPath(std::vector<int> path, int **arrs) {
 }
 
 std::vector<std::vector<int>> gen_mutate(std::vector<std::vector<int>> arrs, std::vector<std::vector<int>> currPaths) {
-  // Start at 1 to keep parent
+  // Loop through all arrays
   for (unsigned int x = 1; x < currPaths.size(); ++x) {
-    // Guarentee mutation
-    unsigned int toChange = rand() % arrs.at(x).size();
-    unsigned int changePlace = rand() % currPaths.at(x).size();
-
-    currPaths.at(x).at(changePlace) = toChange;
-
+    // Loop through all current paths
     for (unsigned int y = 0; y < currPaths.at(x).size(); ++y) {
-      unsigned int toChange = rand() % arrs.at(x).size();
-      unsigned int toChangeCheck = rand() % arrs.at(x).size();
+      unsigned int toChange = rand() % arrs.at(y).size();
+      unsigned int toChangeCheck = rand() % 100;
 
-      if (toChangeCheck < 2) {
+      // x percentage chance of mutation
+      if (toChangeCheck < 10 || y == 0) {
         currPaths.at(x).at(y) = toChange;
       }
     }
@@ -57,7 +53,7 @@ struct gen_trim_struct {
   int fitness;
 };
 
-gen_trim_struct gen_trim(std::vector<std::vector<int>> arrs, std::vector<std::vector<int>> currPaths, int pathNum, int compare(int, int)) {
+gen_trim_struct gen_trim(std::vector<std::vector<int>> arrs, std::vector<std::vector<int>> currPaths, int pathNum, int compare(int, int), int fitness(std::vector<std::vector<int>>*, std::vector<int>, int)) {
   int fitPlaceHigh = 0;
   int fitValueHigh = 0;
 
@@ -79,8 +75,8 @@ gen_trim_struct gen_trim(std::vector<std::vector<int>> arrs, std::vector<std::ve
   return topFound;
 }
 
-std::vector<std::vector<int>> gen_base(std::vector<std::vector<int>> currPaths, std::vector<int> base) {
-  for (unsigned int x = 0; x < base.size(); x++) {
+std::vector<std::vector<int>> gen_base(std::vector<std::vector<int>> currPaths, std::vector<int> base, int groupSize) {
+  for (int x = 0; x < groupSize; x++) {
     currPaths.push_back(base);
   }
 
@@ -104,9 +100,41 @@ float standard_deviation(std::vector<int> fitnesses) {
   return sqrt(var);
 }
 
-void gen(std::vector<std::vector<int>> arrs, int compare(int, int)) {
-  int groups = 15;
-  int cycles = 50;
+bool filt(int a, int b) {
+  if (a == b)
+    return false;
+  return true;
+}
+
+void gen(std::vector<std::vector<int>> arrs, int compare(int, int), int fitness(std::vector<std::vector<int>>*, std::vector<int>, int), bool filter(int, int)) {
+  int groups = 20;
+  int cycles = 150;
+  int groupSize = 20;
+
+  std::vector<std::vector<std::vector<std::vector<bool>>>> possibleCombinations;
+
+  for (unsigned int i = 0; i < arrs.size(); ++i) {
+    std::vector<std::vector<std::vector<bool>>> possibleOuter;
+    for (unsigned int j = 0; j < arrs.at(i).size(); ++j) {
+      int curr = arrs[i][j];
+
+      std::vector<std::vector<bool>> outer;
+      for (unsigned int i2 = 0; i2 < arrs.size(); ++i2) {
+        std::vector<bool> inner;
+        for (unsigned int j2 = 0; j2 < arrs.at(i2).size(); ++j2) {
+          inner.push_back(filter(curr, arrs[i2][j2]));
+        }
+        outer.push_back(inner);
+        inner.clear();
+      }
+      possibleOuter.push_back(outer);
+      outer.clear();
+    }
+    possibleCombinations.push_back(possibleOuter);
+    possibleOuter.clear();
+  }
+
+  std::cout << possibleCombinations[0][0][1][1] << std::endl;
 
   std::vector<std::vector<std::vector<int>>> currPathsGroup;
   std::vector<int> base_path;
@@ -121,7 +149,7 @@ void gen(std::vector<std::vector<int>> arrs, int compare(int, int)) {
   // init
   for (int x = 0; x < groups; ++x) {
     std::vector<std::vector<int>> currPaths;
-    currPaths = gen_base(currPaths, base_path);
+    currPaths = gen_base(currPaths, base_path, groupSize);
 
     currPathsGroup.push_back(currPaths);
   }
@@ -132,16 +160,7 @@ void gen(std::vector<std::vector<int>> arrs, int compare(int, int)) {
       currPathsGroup.at(x) = gen_mutate(arrs, currPathsGroup.at(x));
     }
 
-    // trim
-    std::vector<int> endCondition;
-    for (int x = 0; x < groups; ++x) {
-      gen_trim_struct topGen = gen_trim(arrs, currPathsGroup.at(x), x, compare);
-
-      currPathsGroup.at(x).clear();
-      currPathsGroup.at(x) = gen_base(currPathsGroup.at(x), topGen.top);
-    }
-
-    /*for (auto x : currPathsGroup) {
+    for (auto x : currPathsGroup) {
       for (unsigned int i = 0; i < x.size(); ++i) {
         for (auto z : x.at(i)) {
           std::cout << z << ", ";
@@ -149,7 +168,16 @@ void gen(std::vector<std::vector<int>> arrs, int compare(int, int)) {
       std::cout << std::endl;
       }
       std::cout << "_______________________" << std::endl;
-    }*/
+    }
+
+    // trim
+    std::vector<int> endCondition;
+    for (int x = 0; x < groups; ++x) {
+      gen_trim_struct topGen = gen_trim(arrs, currPathsGroup.at(x), x, compare, fitness);
+
+      currPathsGroup.at(x).clear();
+      currPathsGroup.at(x) = gen_base(currPathsGroup.at(x), topGen.top, groupSize);
+    }
 
     std::cout << std::endl;
   }
@@ -158,7 +186,7 @@ void gen(std::vector<std::vector<int>> arrs, int compare(int, int)) {
 int main() {
   srand (time(NULL));
 
-  std::vector<int> arr1 = {100,153,622,906,759,562,  5,243, 91,362,730,253,383,361,423,219,987,747,492,991};
+  std::vector<int> arr1 = {153,622,906,759,562,5 ,243, 91,362,730,253,383,361,423,219,987,747,492,991};
   std::vector<int> arr2 = {319,752,704,272,755,666,630,137,245,790,806,665,344,890, 58,953,819,554,358,529};
   std::vector<int> arr3 = {217,289,406,587,375,534,569,133,394,171,363,206,810,743, 15,654,669,437,392, 66};
   std::vector<int> arr4 = {209,230,250,779,280,892,783, 42,655,258,365,595,125,737,159,168,315, 89,717,285};
@@ -168,15 +196,15 @@ int main() {
 
   std::vector<std::vector<int>> arrs = {arr1, arr2, arr3, arr4, arr5, arr6, arr7};
 
-  /*std::vector<int> arr1 = {1,0,0,0};
+  /*std::vector<int> arr1 = {1,0,0};
   std::vector<int> arr2 = {0,2,0,0};
   std::vector<int> arr3 = {0,0,3,0};
   std::vector<int> arr4 = {0,0,0,4};
   std::vector<std::vector<int>> arrs = {arr1, arr2, arr3, arr4};*/
 
-  gen(arrs, comp);
+  gen(arrs, comp, fit, filt);
 
-  test1();
+  //test1();
 
   return 0;
  }
